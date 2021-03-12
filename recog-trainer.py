d95111bfabd368a53fdff2e2c375707c61bf7213
anigonzales <anisgonzales@gmail.com>
2021-03-12 16:57:07 -0600

    complete trainer recognizer

--- recog-trainer.py
+++ recog-trainer.py
@@ -1,48 +1,59 @@
-import os
-import numpy as np 
-from PIL import Image 
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import numpy as np 
 import pickle
+import RPi.GPIO as GPIO
+from time import sleep
 
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-recognizer = cv2.face.LBPHFaceRecognizer_create()
+relay_pin = [26]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(relay_pin, GPIO.OUT)
+GPIO.output(relay_pin, 0)
 
-baseDir = os.path.dirname(os.path.abspath(__file__))
-imageDir = os.path.join(baseDir, "images")
-
-currentId = 1
-labelIds = {}
-yLabels = []
-xTrain = []
-
-for root, dirs, files in os.walk(imageDir):
-	print(root, dirs, files)
-	for file in files:
-		print(file)
-		if file.endswith("png") or file.endswith("jpg"):
-			path = os.path.join(root, file)
-			label = os.path.basename(root)
-			print(label)
-
-			if not label in labelIds:
-				labelIds[label] = currentId
-				print(labelIds)
-				currentId += 1
-
-			id_ = labelIds[label]
-			pilImage = Image.open(path).convert("L")
-			imageArray = np.array(pilImage, "uint8")
-			faces = faceCascade.detectMultiScale(imageArray, scaleFactor=1.1, minNeighbors=5)
-
-			for (x, y, w, h) in faces:
-				roi = imageArray[y:y+h, x:x+w]
-				xTrain.append(roi)
-				yLabels.append(id_)
-
-with open("labels", "wb") as f:
-	pickle.dump(labelIds, f)
+with open('labels', 'rb') as f:
+	dicti = pickle.load(f)
 	f.close()
 
-recognizer.train(xTrain, np.array(yLabels))
-recognizer.save("trainer.yml")
-print(labelIds)
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+recognizer = cv2.face.createLBPHFaceRecognizer()
+recognizer.read("trainer.yml")
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	frame = frame.array
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
+	for (x, y, w, h) in faces:
+		roiGray = gray[y:y+h, x:x+w]
+
+		id_, conf = recognizer.predict(roiGray)
+
+		for name, value in dict.items():
+			if value == id_:
+				print(name)
+
+		if conf <= 70:
+			GPIO.output(relay_pin, 1)
+			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+			cv2.putText(frame, name + str(conf), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
+
+		else:
+			GPIO.output(relay_pin, 0)
+
+	cv2.imshow('frame', frame)
+	key = cv2.waitKey(1)
+
+	rawCapture.truncate(0)
+
+	if key == 27:
+		break
+
+cv2.destroyAllWindows()
